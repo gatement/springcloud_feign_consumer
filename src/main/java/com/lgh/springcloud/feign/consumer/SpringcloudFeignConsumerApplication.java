08412ccce819d3501c8bcda7ff4338763cb45536
Johnson Lau <gatement@gmail.com>
2018-02-21 09:30:30 +0800

    go

---
+++ src/main/java/com/lgh/springcloud/feign/consumer/SpringcloudFeignConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.lgh.springcloud.feign.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudFeignConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudFeignConsumerApplication.class, args);
+	}
+}
