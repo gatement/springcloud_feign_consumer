df926c1e8c040bbb3c0ba9743d12f34ecd6d3139
Johnson Lau <gatement@gmail.com>
2018-02-21 14:23:31 +0800

    go

--- src/main/java/com/lgh/springcloud/feign/consumer/SpringcloudFeignConsumerApplication.java
+++ src/main/java/com/lgh/springcloud/feign/consumer/SpringcloudFeignConsumerApplication.java
@@ -2,8 +2,12 @@ package com.lgh.springcloud.feign.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class SpringcloudFeignConsumerApplication {
 
 	public static void main(String[] args) {
